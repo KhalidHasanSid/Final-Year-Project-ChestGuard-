187ca9eae394abfa42c6b27c1a67c010a7e91f6a
KhalidHasanSid <khalidhasansiddiqui21@gmail.com>
2025-02-05 20:38:04 +0500

    ammaz ny home bna dia 241

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
 export default function Home() {
-  return (
-    <>
-    <p>hello</p>
-    </>
-  )
-}
+  return (<>
+   < div className="min-h-screen bg-gradient-to-b from-blue-900 to-indigo-700 text-white p-6 mt-7">
+   <main className="flex flex-col lg:flex-row items-center justify-center gap-8 mt-10">
+  
+    <img
+          src="https://www.pdcenterlv.com/wp-content/uploads/2019/07/home-highlights-lungs.png"
+          alt="Chest X-ray"
+          className="rounded-xl shadow-lg w-96 h-auto"
+        />
+
+   <div className="max-w-md">
+       <h1 className="text-4xl font-extrabold mb-4">Welcome to CGD Dashboard</h1>
+       <p className="text-lg leading-relaxed">
+         This system helps detect chest conditions like <strong>pneumonia</strong> and <strong>tuberculosis</strong> 
+         using AI-powered algorithms. Upload your chest X-ray, enter clinical symptoms, 
+         and let the system predict possible diseases based on the data provided.
+       </p>
+     </div>
+   </main>
+   </div>
+    
+  
+  </>)}
+   
+
+   
+
+  
+
+
+   
+  
+
