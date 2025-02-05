217e008320ffac1e03df1c11690e6ea0e36e68c9
KhalidHasanSid <khalidhasansiddiqui21@gmail.com>
2025-02-05 20:06:08 +0500

    nav bar ko color krdia h aur usky nut bold tight keay hain

--- frontend/src/header.jsx
+++ frontend/src/header.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <>
-    <p>yahan nav pr apni marwai ga </p>
-    
-    </>
-  )
+    <nav className="bg-[#087198] py-3 px-6 rounded-full w-3/4 mx-auto mt-7">
+      <div className=" rounded-full w-3/4 ml-auto">
+        <ul className="flex space-x-6 justify-center text-lg">
+          <li>
+            <NavLink
+              to="/detection"
+              className="text-white hover:text-gray-200 transition"
+            >
+              Detection
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/askQuestion"
+              className="text-white hover:text-gray-200 transition"
+            >
+              Ask Question
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/faqs" className="text-white hover:text-gray-200 transition">
+              FAQs
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/faqs" className="text-white hover:text-gray-200 transition">
+              Article&Researches
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
--- frontend/src/layout.jsx
+++ frontend/src/layout.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './header.jsx'
 
-
 export default function Layout() {
-  return (
-    <div>
-        <Header/>
-        <Outlet/>
-    </div>
+  return (<>
+    
+          
+          <Header/>
+      
+          <Outlet/>
+        
+      
+   
+    </>
   )
 }
